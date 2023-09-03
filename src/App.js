fb18f8f348b1b97a2b0d5d564c651372e06398bc
BH V KRISHNA PRASAD <113228728+krishnap1312@users.noreply.github.com>
2023-09-03 14:39:22 +0530

    reactwebsite

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./Components/HomeOfComponents";
+import Instagram from "./Components/Instagram";
+import GitHub from "./Components/GitHub";
+import LinkedIn from "./Components/LinkedIn";
+import WhatsApp from "./Components/WhatsApp";
+
+
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/Instagram" element={<Instagram />} />
+      <Route path="/Whatsapp" element={<WhatsApp />} />
+      <Route path="/LinkedIn" element={<LinkedIn/>} />
+      <Route path="/GitHub" element={<GitHub />} />
+    </Routes>
+  </BrowserRouter>
   );
 }
 
